c581698ae37a7b2eeae91e6b15b27d069093aa02
Khalil's MAC <khalilobedjaoui@gmail.com>
2022-11-22 12:00:08 +0100

    Comments translation

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class GameState
 {
-    public const int Rows = 8;
-    public const int Cols = 8;
+    public const int Rows = 8; // Nombre de ligne
+    public const int Cols = 8; // Nombre de colonne 
     
-    public PlayerEnum[,] Board { get; } // 2D Array which is the game board
-    public Dictionary<PlayerEnum, int> DiscCount { get; } // The number of discs that each player possesses
-    public PlayerEnum CurrentPlayer { get; private set; } // Which player is currenlty playing
-    public bool GameOver { get; private set; } // Bool to determine whether the game is over or not
-    public PlayerEnum Winner { get; private set; } // Which player has won
-    public Dictionary<PlayerPosition, List<PlayerPosition>> LegalMoves { get; private set; } // The move and a list of all the Taken Discs.
-
+    public PlayerEnum[,] Board { get; } // Un tableau à deux dimensions correspondant au plateau de jeu
+    public Dictionary<PlayerEnum, int> DiscCount { get; } // Le nombre de pions que chaque joueur possède
+    public PlayerEnum CurrentPlayer { get; private set; } // Le joueur actuellement en train de jouer
+    public bool GameOver { get; private set; } // Permettant de déterminer si le jeu est finit ou non
+    public PlayerEnum Winner { get; private set; } // Quel joueur a gagné
+    public Dictionary<PlayerPosition, List<PlayerPosition>> LegalMoves { get; private set; } // Un ajout de pion et la liste des pions adverses que le pion va prednre
+	
     public GameState()
     {
-        Board = new PlayerEnum[Rows, Cols]; // Initialize the Board
-        Board[3, 3] = PlayerEnum.White; // The 4 discs already positioned at the beginning of the game
+        Board = new PlayerEnum[Rows, Cols]; // Initialisation du plateau
+        Board[3, 3] = PlayerEnum.White; // La position des pions au début du jeu
         Board[3, 4] = PlayerEnum.Black;
         Board[4, 3] = PlayerEnum.Black;
         Board[4, 4] = PlayerEnum.White;
 
-        DiscCount = new Dictionary<PlayerEnum, int>() // Each player has 2 discs on the board
+        DiscCount = new Dictionary<PlayerEnum, int>() // Chaque joueur a deux pions sur le plateau au début
         {
             {PlayerEnum.Black, 2},
             {PlayerEnum.White, 2}
         };
 
-        CurrentPlayer = PlayerEnum.Black; // The first player to play, according to the rules, is the one using the black discs
+        CurrentPlayer = PlayerEnum.Black; // Selon les règles, le premier joueur est le joueur possèdant les pions noirs
     }
 
     private bool IsInsideBoard(int r, int c)
